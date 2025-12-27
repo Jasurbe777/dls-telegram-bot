2c501290fdba7c651546efe7afe74d14384391f1
Jasurbek <jasurbekibrohimov01@gmail.com>
2025-12-27 23:32:22 +0500

    Fix: add already_sent helper

--- main.py
+++ main.py
@@ -86,6 +86,8 @@ async def start(message: types.Message, state: FSMContext):
 
     text = (
         "Salom, bu DLS ISMOILOV konkursida qatnashish uchun yaratilgan bot ✅\n\n"
+        "Rasm yuborishda faqat o'zingizni akkauntingizni rasmini yuboring."
+        "Biz marta rasm yuborish imkoniyatingiz bor holos ❗️"
         "Botdagi shartlarga rioya qiling va konkursda bemalol qatnashavering ❗️"
     )
 
